094b2256535c977dbb071eaa0b8402febd5a395d
Fabricio Pereira Fonseca <f.pereirafonseca@gmail.com>
2023-10-28 16:32:47 +0100

    ready to deploy, minor customizations to be made.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,28 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid 
+border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b>f.pereirafonseca@gmail.com</p>
+        <p><b class="pr-2">Linkedin</b>
+            <a href="https://linkedin/in/fpfonseca" 
+            target="_blank" class="text-violet-400"
+            > 
+                Fabricio Fonseca<sup class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                </sup>
+            </a>
+        </p>
+        <p><b class="pr-2">Github</b>
+            <a href="https://github.com/FbrFonseca" 
+            target="_blank" class="text-violet-400"
+            > 
+                Fabricio Fonseca<sup class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                </sup>
+            </a>
+        </p>
+    </div>
+</footer>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -7,6 +7,12 @@
         {name:'Projecto3', icon:'fa-solid fa-diagram-project'},
     ];
 
+    let benefits = [
+        {name: 'a self taught developer', description: 'My favourite tech includes JavaScript Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda explicabo ab obcaecati totam, tenetur atque, nemo iure debitis qui officiis soluta mollitia itaque earum impedit alias! Quis, qui! Necessitatibus, molestias!'},
+        {name: 'a product design & UX fanatic', description: 'My favourite tech includes JavaScript Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda explicabo ab obcaecati totam, tenetur atque, nemo iure debitis qui officiis soluta mollitia itaque earum impedit alias! Quis, qui! Necessitatibus, molestias!'},
+        {name: 'an excelet communicator', description: 'My favourite tech includes JavaScript Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda explicabo ab obcaecati totam, tenetur atque, nemo iure debitis qui officiis soluta mollitia itaque earum impedit alias! Quis, qui! Necessitatibus, molestias!'},
+    ];
+
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -25,7 +31,7 @@
             <a 
                 href="https://www.linkedin.com/in/fpfonseca"
                 target="_blank"
-                class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
             >
                 <div 
                     class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
@@ -96,7 +102,143 @@
 
     </section>
 
-    <section dir="about">
+    <section dir="about" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col
+    gap-16 sm:gap-20 md:gap-24 relative">
+        
+        <div class="flex flex-col gap-2 text-center relative before:absolute 
+        before:top-0 before:left-0 before:w-2/3 before:h-1.5
+        before:bg-violet-700 after:absolute 
+        after:bottom-0 after:right-0 after:w-2/3 after:h-1.5
+        after:bg-violet-700 py-4">
+            <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about </span>me.
+            </h3>
+        </div>
+        
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am . . .</p>
+        
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+           
+            {#each benefits as benefit, index}
+
+                <div class="flex gap-6 sm:gap-8">
+
+                    <p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold">
+                        0{index+1}
+                    </p>
+
+                    <div class="flex flex-col gap-6 sm:gap-8">
+
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+
+                        <p>{benefit.description}</p>
+
+                    </div>
+
+                </div>
+           
+            {/each}
+       
+        </div>
+
+        <h5 class="text-2xl sm:text-3xl font-semibold text-center poppins">The <span class="text-violet-500 poppins">Complete</span> Package</h5>
+        
+        <div class="flex flex-col overflow-x-scroll gap-10 max-w-[800] mx-auto w-full">
+            <table class="bg-white text-slate-700 rounded text-center">
+                <thead class="border-b border-solid border-slate-200">
+                    <tr class="">
+                        <th/>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+                        <th class="whitespace-nowrap bg-violet-700 p-4 px-8 text-white">Me</th>
+                    </tr>
+                   
+                </thead>
+                <tbody>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">
+                            Dedication
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-check text-green-500"></i>
+                        </td>
+
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">
+                            Critical Though
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-check text-green-500"></i>
+                        </td>
+
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">
+                            Interparsonal Skills
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-check text-green-500"></i>
+                        </td>
+
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">
+                            Programming Ability
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-xmark text-slate-500"></i>
+                        </td>
+                        <td>
+                            <i class="fa-solid fa-check text-green-500"></i>
+                        </td>
+
+                    </tr>
+                </tbody>
+            </table>        
+        </div>
+
+        <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+            <p>Scroll to see more &rarr;</p>
+        </div>
+        <p class="mx-auto">So why not invest?</p>
 
     </section>
 
